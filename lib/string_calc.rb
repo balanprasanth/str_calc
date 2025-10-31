92e3cc590e3605cfb240ff1e73824391a536a863
balanprasanth <balanp@google.com>
2025-10-31 21:49:54 +0530

    Fix: Correct all error message

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -1,6 +1,35 @@
 class StringCalc
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(',').map(&:to_i).sum
+    
+    delimiter, numbers_string = extract_val(numbers)
+    number_array = parse_numbers(numbers_string, delimiter)
+    
+    validate_no_negatives(number_array)
+    
+    number_array.sum
+  end
+  
+  private
+  
+  def extract_val(numbers)
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2..-1]
+      [delimiter, parts[1]]
+    else
+      [',', numbers]
+    end
+  end
+  
+  def parse_numbers(numbers, delimiter)
+    numbers.split(/[#{Regexp.escape(delimiter)}\n]/).map(&:to_i)
+  end
+  
+  def validate_no_negatives(numbers)
+    negatives = numbers.select { |n| n < 0 }
+    unless negatives.empty?
+      raise ArgumentError, "negative numbers not allowed: #{negatives.join(', ')}"
+    end
   end
 end
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -2,20 +2,71 @@ require 'spec_helper'
 require_relative '../lib/string_calc'
 
 RSpec.describe StringCalc do
-  let(:calculator) { StringCalculator.new }
+  let(:calculator) { StringCalc.new }
   
-    it 'returns the num itself for a single num' do
-    calculator = StringCalc.new
-    expect(calculator.add("1")).to eq(1)
+  describe '#add' do
+    context 'with empty string' do
+      it 'returns 0' do
+        expect(calculator.add("")).to eq(0)
+      end
     end
-
-    it 'returns sum of two comma-separated numbers' do
-    calculator = StringCalc.new
-    expect(calculator.add("1,2")).to eq(3)
+    
+    context 'with single number' do
+      it 'returns the number itself' do
+        expect(calculator.add("1")).to eq(1)
+        expect(calculator.add("5")).to eq(5)
+      end
     end
-
-    it 'returns sum of multiple comma-separated numbers' do
-    calculator = StringCalc.new
-    expect(calculator.add("1,2,3,4,5")).to eq(15)
+    
+    context 'with two numbers' do
+      it 'returns their sum' do
+        expect(calculator.add("1,2")).to eq(3)
+        expect(calculator.add("10,20")).to eq(30)
+      end
     end
+    
+    context 'with multiple numbers' do
+      it 'returns the sum of all numbers' do
+        expect(calculator.add("1,2,3")).to eq(6)
+        expect(calculator.add("1,2,3,4,5")).to eq(15)
+      end
+    end
+    
+    context 'with newline delimiter' do
+      it 'handles newlines between numbers' do
+        expect(calculator.add("1\n2,3")).to eq(6)
+      end
+    end
+    
+    context 'with custom delimiter' do
+      it 'supports custom delimiter specified at the beginning' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+      
+      it 'supports different custom delimiters' do
+        expect(calculator.add("//|\n1|2|3")).to eq(6)
+        expect(calculator.add("//***\n1***2***3")).to eq(6)
+      end
+    end
+    
+    context 'with negative numbers' do
+      it 'throws an exception for a single negative number' do
+        expect { calculator.add("-1,2") }.to raise_error(
+          ArgumentError, "negative numbers not allowed: -1"
+        )
+      end
+      
+      it 'shows all negative numbers in the exception message' do
+        expect { calculator.add("1,-2,3,-4") }.to raise_error(
+          ArgumentError, "negative numbers not allowed: -2, -4"
+        )
+      end
+      
+      it 'works with custom delimiter and negative numbers' do
+        expect { calculator.add("//;\n1;-2;3") }.to raise_error(
+          ArgumentError, "negative numbers not allowed: -2"
+        )
+      end
+    end
+  end
 end
